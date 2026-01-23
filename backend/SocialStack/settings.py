354acf8dc8ad1cab547eb405ea6b080c168a95ad
aadarsh <yenamandraaadarsh2508@gmail.com>
2026-01-23 16:12:42 +0530

    [MODIFY] settings file database configuration

--- backend/SocialStack/settings.py
+++ backend/SocialStack/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER", "social_stack"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST", "db"),
+        "HOST": os.getenv("POSTGRES_HOST"),
         "PORT": os.getenv("POSTGRES_PORT", 5432),
     }
 }
